96e943fca65b42f71a65fa8d33643dbca194ccf4
Nanda-maker <nandakrgurung123@gmail.com>
2024-02-08 22:22:13 +0600

    global font style configuration

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -12,4 +13,15 @@ export default async function Home() {
       <ProductCard />
     </main>
   );
+}
+
+// export const metadata:Metadata ={
+//   title:'.. '
+// }
+//generate meta data dynamically
+export async function generateMetadata(): Promise<Metadata> {
+  const product = await fetch("");
+  return {
+    title: "product.title",
+  };
 }
